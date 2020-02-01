69827edee279851246b2fc59e2a48c7d5d38f488
土田拓 <taku_tsuchida@tsuchitatakunoMacBook-Air.local>
2020-02-01 10:41:20 +0900

    downgrade of mysql to 5.7

--- app/go.mod
+++ app/go.mod
@@ -3,7 +3,8 @@ module app
 go 1.13
 
 require (
-	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/jinzhu/gorm v1.9.12
 	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/gommon v0.3.0 // indirect
 	golang.org/x/crypto v0.0.0-20200109152110-61a87790db17 // indirect
---
+++ app/handler/auth.go
@@ -0,0 +1,48 @@
+package handler
+
+import (
+	"app/model"
+	"net/http"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
+
+type jwtCustomClaims struct {
+	UID  int    `json:"uid"`
+	Name string `json:"name"`
+	jwt.StandardClaims
+}
+
+var signingKey = []byte("secret")
+
+// Config はJWTConfigで設定します
+var Config = middleware.JWTConfig{
+	Claims:     &jwtCustomClaims{},
+	SigningKey: signingKey,
+}
+
+// Signup function of Signup
+func Signup(c echo.Context) error {
+	user := new(model.User)
+	if err := c.Bind(user); err != nil {
+		return err
+	}
+	if user.Name == "" || user.Password == "" {
+		return &echo.HTTPError{
+			Code:    http.StatusBadRequest,
+			Message: "invalid name or password",
+		}
+	}
+	if u := model.FindUser(&model.User{Name: user.Name}); u.ID != 0 {
+		return &echo.HTTPError{
+			Code:    http.StatusConflict,
+			Message: "name already exists",
+		}
+	}
+	model.CreateUser(user)
+	user.Password = ""
+
+	return c.JSON(http.StatusCreated, user)
+}
