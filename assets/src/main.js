96c4184c96deb2aa4fe6bf3530756839070d1fdd
土田拓 <taku_tsuchida@tsuchitatakunoMacBook-Air.local>
2020-02-11 14:26:39 +0900

    install vue

--- assets/src/main.js
+++ assets/src/main.js
@@ -1,8 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App'
+import router from '@/router/router.js';
+import vuetify from './plugins/vuetify';
 
-Vue.config.productionTip = false
+Vue.use(Vuetify);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    el: '#app',
+    router,
+    vuetify,
+    render: h => h(App)
+})
