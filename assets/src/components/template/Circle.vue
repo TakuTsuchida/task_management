b2864f7f9c41917226a2d8e647e8b5de6ea5b64b
土田拓 <taku_tsuchida@tsuchitatakunoMacBook-Air.local>
2020-03-20 14:58:43 +0900

    draft

---
+++ assets/src/components/template/Circle.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container-fluid">
+	<div>
+		<div class="col-xs-12">ヘッダー</div>
+	</div>
+	<div>
+		<div class="col-xs-12 col-md-3">メニューＡ</div>
+		<div class="col-xs-12 col-sm-8 col-md-6" style="height: 9em;">メンコンテンツ</div>
+		<div class="col-xs-12 col-sm-4 col-md-3">メニューＢ</div>
+	</div>
+	<div>
+		<div class="col-xs-12">フッター</div>
+	</div>
+</div>
+</template>
+
